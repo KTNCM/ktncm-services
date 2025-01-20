c39a1b3312dce21aef93ebdb2f86d3c7e6490473
Gerixmus <gerixmuss@gmail.com>
2025-01-21 00:37:31 +0100

    docs: added docstrings to tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -7,21 +7,33 @@ class TestDatabase(unittest.TestCase):
     @patch('mysql.connector.connect', return_value = MagicMock())
     def test_connect_successful(self, mock_connect: MagicMock):
         """Test that the database connection is established successfully."""
+        # Arrange
         db = Database()
+
+        # Act
         db.connect()
+
+        # Assert
         self.assertIsNotNone(db.connection)
         mock_connect.assert_called_once()
 
     @patch('mysql.connector.connect')
     def test_connect_unsuccessful(self, mock_connect: MagicMock):
         """Test that the database connection is established unsuccessfully."""
+        # Arrange
         mock_connect.side_effect = mysql.connector.Error()
         db = Database()
+
+        # Act
         db.connect(2, 0)
+
+        # Assert
         self.assertIsNone(db.connection)
         self.assertEqual(mock_connect.call_count, 2)
 
     def test_insert_destinations(self):
+        """Test data insertion."""
+        # Arrange
         mock_connection = MagicMock()
         mock_cursor = MagicMock()
         mock_connection.cursor.return_value = mock_cursor
@@ -30,9 +42,10 @@ class TestDatabase(unittest.TestCase):
         db = Database()
         db.connection = mock_connection
 
+        # Act
         db.insert_destinations(excursion_destinations)
 
+        # Assert
         mock_cursor.executemany.assert_called_once()
-        
         mock_connection.commit.assert_called_once()
         
--- tests/test_destination_service.py
+++ tests/test_destination_service.py
@@ -7,6 +7,11 @@ from source.destination import Destination
 class TestDestinationService(unittest.TestCase):
     @patch('source.destinations_service.return_soup', return_value = MagicMock())
     def test_update_info_calls_return_soup(self, mock_return_soup: MagicMock):
+        """Verify that return_soup is called within update_info.
+
+        Args:
+            mock_return_soup (MagicMock): Mock for return_soup function.
+        """
         # Arrange
         destination = Destination()
         destination.url = "http://mock-url.com"
@@ -19,6 +24,11 @@ class TestDestinationService(unittest.TestCase):
 
     @patch('source.destinations_service.return_soup', return_value = MagicMock())
     def test_fetch_destinations_calls_return_soup(self, mock_return_soup: MagicMock):
+        """Verify that return_soup is called within fetch_destination.
+
+        Args:
+            mock_return_soup (MagicMock): Mock for return_soup function.
+        """
         # Arrange
         url = "https://www.kaerntencard.at/sommer/en/ausflugsziele-uebersicht/"
 
